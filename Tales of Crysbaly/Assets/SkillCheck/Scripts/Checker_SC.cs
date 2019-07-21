d85efda39395853d6cae4ea37f6ba7423ce7ee61
Punkid07 <ayrloncrack@hotmail.com>
2019-07-21 17:54:23 -0500

    Integración Gameplay SkillCheck
    Se agrego el package que contiene el elemento de gameplay de SkillCheck, consiste en ir hacer skillcheck en 4 carriles

---
+++ Tales of Crysbaly/Assets/SkillCheck/Scripts/Checker_SC.cs
@@ -0,0 +1,251 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Checker_SC : MonoBehaviour
+{
+    [Header("Variables")]
+    public Tecla tecla;
+    public bool EnRaya = false;
+    public float Velocidad = 1;
+    public GameObject ParticulasAtinar;
+    public GameObject ParticulasNoAtinar;
+    [Space(5)]
+
+    [Header("Teclas")]
+    public bool Btn_A = false;
+    public bool Btn_W = false;
+    public bool Btn_S = false;
+    public bool Btn_D = false;
+    [Space(5)]
+
+    [Header("ControlSkillCheck")]
+    public ControlSkillCheck_SC Control; 
+
+
+    //Variables Privadas
+    TextMeshProUGUI LetraRender;
+    Transform _posRaya = null;
+
+    //debugs
+    bool vivo = true;    
+
+    //Teclas
+    public enum Tecla
+    {
+        A,W,S,D
+    }
+
+    #region Inicio
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Asignar renderer
+        LetraRender = GetComponentInChildren<TextMeshProUGUI>();
+
+        //Inicializar el checker
+        SeleccionTecla();
+    }
+
+    //Hacer seleccion aleatoria de tecla
+    void SeleccionTecla()
+    {
+        //Seleccionar y assignar tecla
+        int _random = Random.Range(0, 4);
+        switch (_random)
+        {
+            case 0:
+                tecla = Tecla.A;
+                break;
+            case 1:
+                tecla = Tecla.W;
+                break;
+            case 2:
+                tecla = Tecla.S;
+                break;
+            case 3:
+                tecla = Tecla.D;
+                break;
+            default:
+                tecla = Tecla.D;
+                break;
+        }
+
+        //Poner letra en renderer
+        LetraRender.text = tecla.ToString();
+    }
+    #endregion
+
+    #region Update
+    // Update is called once per frame
+    void Update()
+    {
+        Movimiento();
+        DetectarInput();
+    }
+
+    //Movimiento del Checker
+    void Movimiento()
+    {
+        //mover hacia la derecha
+        transform.Translate(new Vector3(Velocidad, 0, 0));
+    }
+    
+    void DetectarInput()
+    {
+        Btn_A = Input.GetButtonDown("BotonA");
+        Btn_W = Input.GetButtonDown("BotonW");
+        Btn_S = Input.GetButtonDown("BotonS");
+        Btn_D = Input.GetButtonDown("BotonD");
+
+        //Si esta fuera de raya
+        if ((Btn_A || Btn_W || Btn_S || Btn_D) && !EnRaya)
+        {
+            Desctruccion("Se adelanto!");
+
+            return;
+        }else
+
+        //Si esta en raya
+        if(EnRaya)
+        {
+            //Tecla A
+            if(tecla == Tecla.A)
+            {
+                //Acerto -----------------------------------------!
+                if (Btn_A)
+                {
+                    Desctruccion("Atino!");
+                }
+                //Tecla incorrecta
+                else if (Btn_W || Btn_S || Btn_D)
+                {
+                    Desctruccion("Tecla Incorrecta!");
+                    return;
+                }                
+            }else
+
+            //Tecla W
+            if (tecla == Tecla.W)
+            {
+                //Acerto
+                if (Btn_W)
+                {
+                    Desctruccion("Atino!");
+                }
+                //Tecla incorrecta
+                else if (Btn_A || Btn_S || Btn_D)
+                {
+                    Desctruccion("Tecla Incorrecta!");
+                    return;
+                }
+            }else
+
+            //Tecla W
+            if (tecla == Tecla.S)
+            {
+                //Acerto
+                if (Btn_S)
+                {
+                    Desctruccion("Atino!");
+                }
+                //Tecla incorrecta
+                else if (Btn_A || Btn_W || Btn_D)
+                {
+                    Desctruccion("Tecla Incorrecta!");
+                    return;
+                }
+            }
+
+            //Tecla W
+            if (tecla == Tecla.D)
+            {
+                //Acerto
+                if (Btn_D)
+                {
+                    Desctruccion("Atino!");
+                }
+                //Tecla incorrecta
+                else if (Btn_A || Btn_S || Btn_W)
+                {
+                    Desctruccion("Tecla Incorrecta!");
+                    return;
+                }
+            }
+            
+        }
+    }
+    #endregion
+
+    #region Deteccion de raya
+    //Detectar raya o final INICIO
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //esta tocando la raya
+        if(collision.tag == "Raya")
+        {
+            EnRaya = true;
+
+            _posRaya = collision.gameObject.transform;
+        }
+    }
+
+    //Detectar raya FINAL
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //Esta saliendo de la raya, destruir el checker
+        if (collision.tag == "Raya")
+        {
+            EnRaya = false;
+            
+            Desctruccion("No atino!");
+        }
+    }
+    #endregion
+
+    void Desctruccion(string _resultado)
+    {
+        if (vivo)
+        {
+            //debug resultado
+            Debug.Log(_resultado);
+
+            //prevencion doble llamada
+            vivo = false;
+
+            //Mover Raya
+            if (_posRaya != null)
+            {
+                Control.MoverRaya(_posRaya);
+            }
+
+            //Spawn nuevo Checker
+            Control.SpawnChecker();
+
+            //Destruir con Fade y deshabilitar este script
+            //Destroy(gameObject, 0.5f);
+            //Destruir en instanciar particulas
+            PonerParticulas();
+            Destroy(gameObject);
+
+            this.enabled = false;
+        }
+    }
+
+    void PonerParticulas()
+    {
+        //Si atino
+        if (EnRaya)
+        {
+            Instantiate(ParticulasAtinar, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            //Si no atino
+            Instantiate(ParticulasNoAtinar, transform.position, Quaternion.identity);
+        }
+    }
+
+    
+}
---
+++ Tales of Crysbaly/Assets/SkillCheck/Scripts/ControlSkillCheck_SC.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlSkillCheck_SC : MonoBehaviour
+{
+    [Header("Prefabs")]
+    public GameObject CheckerPrefab;
+    public GameObject RayaPrefab;
+    [Space(5)]
+
+    [Header("Posiciones de Spawn")]
+    public RectTransform[] PuntosSpawn;
+    [Space(5)]
+
+    [Header("Velocidades de Checkers")]
+    public float RondasParaMultVelocidad = 5;
+    public int rondasPasadas = 0;
+    public float aumentoDeVelocidad = 0.5f;
+    float multiplicador = 1;
+
+    public bool sinCarriles = false;
+
+    //Posiciones de rayas de cada carril
+    //Vector3[] PosCarriles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Esperar un poco a los chekers
+        Invoke("SpawnChecker", 2);
+
+        //Inicializar Rayas
+        InicioRayas();
+    }
+
+    //Spawn de primeras rayas
+    void InicioRayas()
+    {
+        //Inicializar arreglo de posiciones
+        //PosCarriles = new Vector3[PuntosSpawn.Length];
+
+        //Spawn una raya por carril
+        for(int a = 0; a < PuntosSpawn.Length ; a++)
+        {
+            //Generar posicion aleatoria
+            Vector3 _pos = new Vector3(0, Random.Range(0, 40), 0);
+
+            //Generar Raya
+            GameObject _raya = Instantiate(RayaPrefab,Vector3.zero, Quaternion.identity, PuntosSpawn[a].transform.parent);
+            _raya.transform.localPosition = _pos;
+        }
+    }
+
+    //Spawn Checker
+    public void SpawnChecker()
+    {
+        rondasPasadas++;
+        if(rondasPasadas % RondasParaMultVelocidad == 0)
+        {
+            multiplicador += aumentoDeVelocidad;
+        }
+
+        //Escoger un carril no desactivado
+        int Carril = EscogerCarrilNoDesactivado();
+
+        //No spawnear si ya no hay carriles
+        if (sinCarriles)
+        {
+            Debug.Log("Ya no hay carriles!");
+            return;
+        }
+
+        //Poner en los puntos de spawn de cierto carril
+        GameObject Checker = Instantiate(CheckerPrefab, PuntosSpawn[Carril].position, Quaternion.identity,transform);
+        Checker.GetComponent<Checker_SC>().Control = this;
+        Checker.GetComponent<Checker_SC>().Velocidad *= multiplicador;
+    }
+
+    int EscogerCarrilNoDesactivado()
+    {
+        int _activos = 0;
+        //Comprobar que almenos un carril este activo
+        for(int x = 0; x<PuntosSpawn.Length; x++)
+        {
+            if (PuntosSpawn[x].transform.parent.gameObject.active)
+            {
+                _activos++;
+            }
+        }
+        if (_activos == 0)
+        {
+            sinCarriles = true;
+            return 0;
+        }
+
+        int _Carril = 0;
+        for(int a = 0; a < 2; a++)
+        {
+            _Carril = Random.Range(0, PuntosSpawn.Length);
+
+            if (!PuntosSpawn[_Carril].parent.gameObject.active)
+            {
+                a = 0;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return _Carril;
+    }
+
+    //SpawnRaya
+    public void MoverRaya(Transform _posRaya)
+    { 
+    //Mover Raya a la derecha Eje Y
+        _posRaya.transform.localPosition -= new Vector3(0, Random.Range(30, 40), 0);
+
+        //Desactivar carril si la raya esta más atras
+        if(_posRaya.transform.localPosition.y < -135)
+        {
+            _posRaya.transform.parent.gameObject.SetActive(false);
+        }
+    }
+}
