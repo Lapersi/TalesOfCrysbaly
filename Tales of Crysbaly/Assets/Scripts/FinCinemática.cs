0b901e71aa4fd1c8c309934263782908877bc8ce
Punkid07 <ayrloncrack@hotmail.com>
2019-07-29 13:50:09 -0500

    Casi termino de acto 1
    Faltan algunos detalles de animación, pero la base del acto ya esta completa

--- Tales of Crysbaly/Assets/Scripts/FinCinemática.cs
+++ Tales of Crysbaly/Assets/Scripts/FinCinemática.cs
@@ -9,14 +9,33 @@ public class FinCinemática : MonoBehaviour
     public Flowchart Flowchart;
     public VideoPlayer videoPlayer;
     public string nombreSiguienteBlock = "";
-    
+    public bool Saltar = false;
+
+    private void Awake()
+    {
+        float tiempo = (float)videoPlayer.length;
+        Invoke("CheckFinish", tiempo + 0.5f);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) { 
+        if (Input.GetKeyDown(KeyCode.Space) && !Saltar) {
+            Saltar = true;
             gameObject.SetActive(false);
             Flowchart.ExecuteBlock(nombreSiguienteBlock);
         }
+
         
+    }
+
+    void CheckFinish()
+    {
+        if (!videoPlayer.isPlaying && !Saltar)
+        {
+            Saltar = true;
+            gameObject.SetActive(false);
+            Flowchart.ExecuteBlock(nombreSiguienteBlock);
+        }
     }
 }
