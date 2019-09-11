547215023954293c471f780c235311faeb5fa25e
Punkid07 <ayrloncrack@hotmail.com>
2019-09-10 21:22:13 -0500

    Integracion de menu principal

---
+++ Tales of Crysbaly/Assets/MenuPrincipalSC.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipalSC : MonoBehaviour
+{
+    public string NombreDeNuevoJuego = "";
+
+    public void JuegoNuevo()
+    {
+        SceneManager.LoadScene(NombreDeNuevoJuego);
+    }
+}
