09a17939aabad275dfd4761a3ca001ece74f4eb4
Punkid07 <ayrloncrack@hotmail.com>
2019-09-11 14:36:31 -0500

    Implementacion de menu de pausa
    -detalles en el menu principal

---
+++ Tales of Crysbaly/Assets/Escenas/MenuPrincipal/MenuPrincipalSC.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipalSC : MonoBehaviour
+{
+    public string NombreDeNuevoJuego = "";
+    public AudioSource Musica;
+    bool cambiando = false;
+
+    public void JuegoNuevo()
+    {
+        Invoke("CambiarEscena", 1.5f);        
+    }
+
+    void CambiarEscena()
+    {
+        if (!cambiando)
+        {
+            cambiando = true;
+            SceneManager.LoadSceneAsync(NombreDeNuevoJuego,LoadSceneMode.Single);
+        }
+    }
+}
